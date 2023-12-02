fa7b4c2bf7db5fdbd40ebe7c8c6513abc847f109
kuldeep2745 <kuldeeprathore2745@gmail.com>
2023-12-02 15:17:18 +0530

    Shown signup form inside modal

--- src/AuthComponent.js
+++ src/AuthComponent.js
@@ -1,15 +1,13 @@
-import React, { useEffect, useState } from "react";
-import Signup from "./components/auth/Signup";
-import { Button } from "react-bootstrap";
+import React, { useEffect, useState, useContext } from "react";
+import { Button, Modal } from "react-bootstrap";
 import axios from "axios";
 import Cookies from "universal-cookie";
-import UserDetails from "./components/users/UserDetails"
-import UserList from "./components/users/UsersList"
-import {MyContext} from "./MyContext"
-// import { Redirect } from "react-router-dom"; // Import Redirect
-const cookies = new Cookies();
+import Signup from "./components/auth/Signup";
+import UserDetails from "./components/users/UserDetails";
+import UserList from "./components/users/UsersList";
+import { MyContext } from "./MyContext";
 
-// get token generated on login
+const cookies = new Cookies();
 const token = cookies.get("TOKEN");
 
 export default function AuthComponent() {
@@ -17,7 +15,7 @@ export default function AuthComponent() {
   const [isAdmin, setIsAdmin] = useState(false);
   const [userList, setUserList] = useState([]);
   const [userDetails, setUserDetails] = useState([]);
-  // const [redirectToAdmin, setRedirectToAdmin] = useState(false); // New state for redirection
+  const [showSignupModal, setShowSignupModal] = useState(false);
 
   useEffect(() => {
     const configuration = {
@@ -30,28 +28,26 @@ export default function AuthComponent() {
 
     axios(configuration)
       .then((result) => {
-        console.log("result.data.isAdmin", result.data.isAdmin)
         setMessage(result.data.message);
         setIsAdmin(result.data.isAdmin);
-        // Redirect logic
-        // if (result.data.isAdmin) {
-        //   setRedirectToAdmin(true);
-        // }
       })
       .catch((error) => {
-        error = new Error();
+        console.error("Error fetching user:", error);
       });
   }, []);
 
   const logout = () => {
     cookies.remove("TOKEN", { path: "/" });
     window.location.href = "/";
-  }
+  };
 
-  // Redirect if isAdmin is true
-  // if (redirectToAdmin) {
-  //   return <Redirect to="/admin-dashboard" />;
-  // }
+  const handleCreateUser = () => {
+    setShowSignupModal(true);
+  };
+
+  const handleCloseSignupModal = () => {
+    setShowSignupModal(false);
+  };
 
   return (
     <div className="text-center">
@@ -63,22 +59,42 @@ export default function AuthComponent() {
       {/* Displaying different content based on user role */}
       {isAdmin ? (
         <div>
-        <h3 className="text-success">Admin Dashboard Content</h3>
-        <Signup />
-        <MyContext.Provider value={{ userList, setUserList, token }}>
-        <UserList />
-      </MyContext.Provider>
+          <h3 className="text-success">Admin Dashboard Content</h3>
+          <Button variant="primary" onClick={handleCreateUser}>
+            Create New User
+          </Button>
+
+          <MyContext.Provider value={{ userList, setUserList, token }}>
+            <UserList />
+          </MyContext.Provider>
         </div>
-        ) : (
-          <div>
-        <h3 className="text-success">User Dashboard Content</h3>
-        <MyContext.Provider value={{ userDetails, setUserDetails, token }}>
-        <UserDetails />
-      </MyContext.Provider>
-          </div>
+      ) : (
+        <div>
+          <h3 className="text-success">User Dashboard Content</h3>
+          <MyContext.Provider value={{ userDetails, setUserDetails, token }}>
+            <UserDetails />
+          </MyContext.Provider>
+        </div>
       )}
 
       <h3 className="text-danger">{message}</h3>
+
+      {/* Signup Modal */}
+      <Modal show={showSignupModal} onHide={handleCloseSignupModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create New User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <MyContext.Provider value={{ userList, setUserList, token }}>
+            <Signup />
+          </MyContext.Provider>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseSignupModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
